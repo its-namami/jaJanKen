0cbcb5d93e6108b2eb631f92fcd5a91779a3ee25
its-namami <164639260+its-namami@users.noreply.github.com>
2025-05-25 18:58:04 +0200

    feat: refactor most js code and make score incrementing work

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,115 +2,94 @@
 // that button, then choose that thing, as if you were to click it.
 // Obviously create clicking event too, and first and foremost.
 // Change the rounds to unlimited until score of someone is 5 – that's when it ends.
+//
+// What is needed:
+// add background to the score, if you won this round, green – lost – red, tie – yellow
 
-let round;
-let userScore;
-let computerScore;
+let userScore = 0;
+let computerScore = 0;
+const elementUserScore = document.querySelector('#player-score');
+const elementComputerScore = document.querySelector('#computer-score');
 
-function getStringComputerChoice(computerChoice){
+const getStringComputerChoice = function getPredeterminedChoiceStringBasedOnRandomThree(computerChoice) {
   let stringComputerChoice;
+
   switch (computerChoice) {
     case 0:
-      stringComputerChoice = "Rock";
+      stringComputerChoice = "ROCK";
       break;
     case 1:
-      stringComputerChoice = "Paper";
+      stringComputerChoice = "PAPER";
       break;
     case 2:
-      stringComputerChoice = "Scissors";
+      stringComputerChoice = "SCISSORS";
       break;
     default:
-      console.error(new Error("You should not be seeing this; it went beyond the scope"));
-      throw new Error("Program died: critical error");
+      throw new Error(`Unexpected computer choice value: ${computerChoice}`);
   }
+
   return stringComputerChoice;
 }
 
-function restart(customString){
-  const userRestart = confirm(`${customString} Would you like to restart the game?`);
-  switch (userRestart){
-    case true:
-      game();
-      break;
-    case false:
-      round=999;
-      break;
+const getRockOutcome = function (computerChoice) {
+  switch (computerChoice) {
+    case 'ROCK':
+      return 'Tie';
+    case 'PAPER':
+      return 'Lose';
+    case 'SCISSORS':
+      return 'Win';
   }
 }
 
-function noInput() {
-  round--;
-  console.warn("User provided no input");
-  alert("You have provided no input.");
+const getPaperOutcome = function (computerChoice) {
+  switch (computerChoice) {
+    case 'ROCK':
+      return 'Win';
+    case 'PAPER':
+      return 'Tie';
+    case 'SCISSORS':
+      return 'Lose';
+  }
 }
 
-function defaultError() {
-  round--;
-  console.error(new Error("Something went wrong"));
-  alert('Something went wrong');
+const getScissorsOutcome = function (computerChoice) {
+  switch (computerChoice) {
+    case 'ROCK':
+      return 'Lose';
+    case 'PAPER':
+      return 'Win';
+    case 'SCISSORS':
+      return 'Tie';
+  }
 }
 
-function alertPlayer(userChoice, computerChoice, outcome){
-  switch(outcome.toUpperCase()){
-    case "TIE":
-      break;
-    case "WIN":
+const updateUIScore = function () {
+  elementUserScore.innerText = userScore;
+  elementComputerScore.innerText = computerScore;
+}
+
+const updateScore = function updateScoreVariablesAndUI (outcome) {
+  switch (outcome) {
+    case 'Win':
       userScore++;
       break;
-    case "LOSE":
+    case 'Lose':
       computerScore++;
       break;
-    default:
-      defaultError();
+    case 'Tie':
       break;
+    default:
+       throw new Error(`Unexpected outcome: ${outcome}`)  ;
   }
-  alert(`Round ${round}) User: ${userChoice}, Computer: ${computerChoice}, Outcome: ${outcome}`);
+
+  updateUIScore();
 }
 
-function wrongInput(input) {
-  round--;
-  console.warn("User Choice is not valid");
-  alert(`${input} is not a valid input`);
-}
-
-function getRockOutcome(computerChoice) {
-  switch(computerChoice){
-    case 'ROCK':
-      return 'Tie';
-    case 'PAPER':
-      return 'Lose';
-    case 'SCISSORS':
-      return 'Win';
-  }
-}
-
-function getPaperOutcome(computerChoice) {
-  switch(computerChoice){
-    case 'ROCK':
-      return 'Win';
-    case 'PAPER':
-      return 'Tie';
-    case 'SCISSORS':
-      return 'Lose';
-  }
-}
-
-function getScissorsOutcome(computerChoice) {
-  switch(computerChoice){
-    case 'ROCK':
-      return 'Lose';
-    case 'PAPER':
-      return 'Win';
-    case 'SCISSORS':
-      return 'Tie';
-  }
-}
-
-function getGameResult(userChoice, computerChoice){
-  userChoice=userChoice.toUpperCase();
-  computerChoice=computerChoice.toUpperCase();
+const getOutcome = function getOutcomeAndUpdateScore (userChoice, computerChoice) {
   let outcome;
-  switch(userChoice){
+
+  switch (userChoice) {
     case 'ROCK':
       outcome = getRockOutcome(computerChoice);
       break;
@@ -121,69 +100,63 @@ function getGameResult(userChoice, computerChoice){
       outcome = getScissorsOutcome(computerChoice);
       break;
     default:
-      wrongInput(userChoice);
-      return;
+       throw new Error(`Unexpected user choice: ${userChoice}`)  ;
   }
-  alertPlayer(userChoice, computerChoice, outcome); 
+
+  updateScore(outcome);
 }
 
-function newChoices() {
-  round++;
-  const getUserChoice = prompt("Rock, Paper or Scissors?: ");
-  const getComputerChoice = Math.floor(Math.random()*3);
-  const stringComputerChoice = getStringComputerChoice(getComputerChoice);
-  console.log(getUserChoice);
-  if(getUserChoice==='')
-    noInput();
-  else if (getUserChoice==null)
-      round = 999;
-  else
-    getGameResult(getUserChoice, stringComputerChoice);
+const getRoundResult = function (userChoice, computerChoice) {
+  userChoice = userChoice.toUpperCase();
+  computerChoice = computerChoice.toUpperCase();
+  let outcome = getOutcome(userChoice, computerChoice);
+}
+
+const round = function playersChoices(userChoice) {
+  const randomThree = Math.floor(Math.random() * 3);
+  const computerChoice = getStringComputerChoice(randomThree);
+  getRoundResult(userChoice, computerChoice);
 };
 
-function alertCongrats(){
-  if(userScore>computerScore)
-    restart(`Congratulations, you won with the score of ${userScore}-${computerScore}.`);
-  if(userScore<computerScore)
-    restart(`You lost with the score of ${userScore}-${computerScore}.`);
-  if(userScore==computerScore)
-    restart(`Good job, it's a tie with the score of ${userScore}-${computerScore}.`);
+const showResult = function showGameResult(surrendered) {
+  // TO-DO:
+  // show game over screen, maybe in form of a <dialog>
+  // if willingly surrendered, then a bit different one
+  // CTA: restart
 }
 
-function game(){
-  round=0;
-  userScore=0;
-  computerScore=0;
-  // while(userScore < 5 || computerScore < 5){
-  //   newChoices();
-  // }
-  // if(round!==999) alertCongrats();
+const gameOver = function removeBtnsDisplayFinalScoreDialog(surrendered = false) {
+  removeChoiceBtns();
+  showResult(surrendered);
 }
 
-// game();
+const surrender = function usrSurrender() {
+  gameOver(true);
+  // TO-DO:
+  // Display (likewise the game over) in a (<dialog>?)
+  // an encouraging and possibly sympathetic
+  // And then simply continue with the normal restart button
+}
 
-// const rock = document.querySelector('button#rock');
-// const paper = document.querySelector('button#paper');
-// const scissors = document.querySelector('button#scissors');
+const restart = function resetVariablesRestartGame () {
+  // TO-DO
+}
 
-const btnChoices = document.querySelectorAll('button.choice-button');
+const game = function playGameMaxScoreX(userChoice, maxScore = 5) {
+  if (userChoice === 'surrender') return surrender(); // TO-DO: surrender function with new game screen;
 
-let thisE;
+  round(userChoice);
 
-btnChoices.forEach(btn => {
-  btn.addEventListener('click', e => {
-    switch (e.target.attributes[1].nodeValue) {
-      case 'rock':
-        console.log('rock');
-        break;
-      case 'paper':
-        console.log('paper');
-        break;
-      case 'scissors':
-        console.log('scissors');
-        break;
-    }
+  if (userScore >= maxScore || computerScore >= maxScore) gameOver();
+}
+
+const gameBtnChoices = document.querySelectorAll('button.choice-button');
+
+gameBtnChoices.forEach(choiceButton => {
+  choiceButton.addEventListener('click', event => {
+    // Button ID name is logical and will be directly interpreted as user input
+    let thisBtnID = event.target.attributes.id.nodeValue;
+    game(thisBtnID);
   });
 });
-
 
